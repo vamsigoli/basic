d8b177bf5704af02078898a62ab66e9c97789f35
vamsigoli <vamsi.goli@gmail.com>
2013-12-01 23:08:43 -0500

    new learning

--- springcdi/entities/src/main/java/com/vamsi/spring/jpa/entities/Employee.java
+++ springcdi/entities/src/main/java/com/vamsi/spring/jpa/entities/Employee.java
@@ -2,20 +2,41 @@ package com.vamsi.spring.jpa.entities;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
 public class Employee {
-	@SequenceGenerator(name = "EMPLOYEE_ID_GENERATOR", sequenceName = "EMP_SEQ" ,initialValue=10 ,allocationSize=1)
+	@SequenceGenerator(name = "EMPLOYEE_ID_GENERATOR", 
+			sequenceName = "EMP_SEQ" ,initialValue=10 ,allocationSize=1)
 	@Id
 	@GeneratedValue(generator = "EMPLOYEE_ID_GENERATOR")
 	private Integer id;
 	
+	
+	@OneToOne(optional=false, cascade=CascadeType.PERSIST )
+	//by keeping optional as false, the Employee table that gets created wil have PSPACE_ID as not null
+	
+	@JoinColumn(name="PSPACE_ID"  ,unique=true)
+	//by keeping unique as true we are adding a unique constraint on PSPACE_ID in EMPLOYEE table.
+	 
+	private ParkingSpace parking;
+	
+	public ParkingSpace getParking() {
+		return parking;
+	}
+
+	public void setParking(ParkingSpace parking) {
+		this.parking = parking;
+	}
+
 	private String lastName;
 	
 	@Temporal(TemporalType.DATE)
@@ -53,8 +74,10 @@ public class Employee {
 	public Integer getId() {
 		return id;
 	}
+	
+	//see the access restriction. setId is marked private
 
-	public void setId(Integer id) {
+	private void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -87,6 +110,11 @@ public class Employee {
 	}
 
 	@Override
+	//refer to article that says not to override equals and hashcode methods
+	//also it is suggested to use auto generated keys instead of a natural key for a primary key. 
+	//use the natural keys as surrogate keys
+	//the below code assumes all elements to be part of the equals while it should only be id
+	
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
--- springcdi/entities/src/test/java/com/vamsi/spring/jpa/entities/EmployeeTester.java
+++ springcdi/entities/src/test/java/com/vamsi/spring/jpa/entities/EmployeeTester.java
@@ -9,6 +9,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -42,7 +43,7 @@ public class EmployeeTester {
 		this.em = em;
 	}
 
-	private EntityManagerFactory emFactory;
+	private EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("entities");
 	private EntityManager em;
 	private  EntityTransaction mTrx;
 
@@ -58,9 +59,8 @@ public class EmployeeTester {
 	public void setUp() throws Exception {
 		try {
 			
-			setEntityManagerFactory(Persistence.createEntityManagerFactory("entities"));
 			logger.info("Building JPA EntityManager for unit tests. this is application managed em");
-			logger.info("what matters the creation of the em.  not the emf");
+			logger.info("what matters is the creation of the em.  not the emf");
 			setEntityManager(getEntityManagerFactory().createEntityManager());
 			
 			logger.info("Start a new transaction for the test");
@@ -92,6 +92,71 @@ public class EmployeeTester {
 		employee.setSalary(99000);
 		employee.setDateOfHire(Calendar.getInstance().getTime());
 		employee.setEmailAddress("a@b.com");
+		
+		ParkingSpace park1 = new ParkingSpace();
+		park1.setLocation("Near FirstGate");
+		park1.setLot(1);
+		
+		employee.setParking(park1);
+		park1.setEmp(employee);
+		//this assignment need to be explicitly managed
+
+		CriteriaBuilder qb = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Employee> c = qb.createQuery(Employee.class);
+		Root<Employee> employeeRoot = c.from(Employee.class);
+		c.select(employeeRoot);
+
+		TypedQuery<Employee> query = getEntityManager().createQuery(c);
+		
+
+		int beforeCount = query.getResultList().size();
+
+		logger.info("Number of employees in the DB:" + beforeCount);
+
+		getTrx().begin();
+		 getEntityManager().persist(employee);
+		 //getId on employee will return correct value as it is set by the em.
+		 //but if we use merge instead of persist, getId will return null
+		getTrx().commit();
+		
+		logger.info("employee id set on parking object : " + park1.getEmp().getId());
+
+		query = getEntityManager().createQuery(c);
+		
+		List<Employee> employeeList = query.getResultList(); 
+		
+		for (Employee e : employeeList) {
+			logger.info("employee id " + e.getId() );
+			logger.info("employee 's parking lot " + e.getParking() );
+		}
+
+		int afterCount = employeeList.size();
+
+		logger.info("Number of students in the DB:" + afterCount);
+		Assert.assertEquals(afterCount, beforeCount + 1);
+
+	}
+	
+	//@Test(expected=PersistenceException.class)
+	public void createEmployeeWithParking() {
+
+		Employee employee = new Employee();
+		employee.setLastName("Chintal");
+		employee.setJobTitle("Manager");
+		employee.setSalary(99000);
+		employee.setDateOfHire(Calendar.getInstance().getTime());
+		employee.setEmailAddress("a@b.com");
+		
+		Employee employee1 = new Employee();
+		employee1.setLastName("Chintal1");
+		employee1.setJobTitle("Manager1");
+		employee1.setSalary(99000);
+		employee1.setDateOfHire(Calendar.getInstance().getTime());
+		employee1.setEmailAddress("a@b1.com");
+		
+		ParkingSpace park1 = new ParkingSpace();
+		park1.setLocation("Near FirstGate");
+		park1.setLot(1);
 
 		CriteriaBuilder qb = getEntityManager().getCriteriaBuilder();
 		CriteriaQuery<Employee> c = qb.createQuery(Employee.class);
@@ -106,7 +171,17 @@ public class EmployeeTester {
 		logger.info("Number of students in the DB:" + beforeCount);
 
 		getTrx().begin();
-		getEntityManager().persist(employee);
+		
+	    getEntityManager().persist(park1);	
+	    
+	    employee.setParking(park1);
+	    employee1.setParking(park1);
+		
+		
+		 getEntityManager().persist(employee);
+		 getEntityManager().persist(employee1);
+		 //getId on employee will return correct value as it is set by the em.
+		 //but if we use merge instead of persist, getId will return null
 		getTrx().commit();
 
 		query = getEntityManager().createQuery(c);
@@ -119,9 +194,10 @@ public class EmployeeTester {
 
 		int afterCount = employeeList.size();
 
-		logger.info("Number of students in the DB:" + afterCount);
-		Assert.assertEquals(afterCount, beforeCount + 1);
+		logger.info("Number of employees in the DB:" + afterCount);
+		Assert.assertEquals(afterCount, beforeCount + 2);
 
 	}
+
 
 }
