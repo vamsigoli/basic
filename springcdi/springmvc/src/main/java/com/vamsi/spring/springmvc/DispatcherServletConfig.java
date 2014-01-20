19c5e3e015b23a9caae8637aa10003b4d9370ef3
vamsigoli <vamsi.goli@gmail.com>
2014-01-20 17:33:29 -0500

    updates. need rework

--- springcdi/springmvc/src/main/java/com/vamsi/spring/springmvc/DispatcherServletConfig.java
+++ springcdi/springmvc/src/main/java/com/vamsi/spring/springmvc/DispatcherServletConfig.java
@@ -1,15 +1,23 @@
 package com.vamsi.spring.springmvc;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.vamsi.spring.springmvc.controller.NomineeController;
+
 @Configuration
 @EnableWebMvc
-//this registers all the default handlermappings and handleradapters to the context
-
+// this registers all the default handlermappings and handleradapters to the
+// context
 @ComponentScan("com.vamsi.spring.springmvc")
 public class DispatcherServletConfig {
 	@Bean
@@ -19,6 +27,27 @@ public class DispatcherServletConfig {
 		result.setSuffix(".jsp");
 		return result;
 	}
-	
-	
+
+	/*
+	 * @Bean public NomineeController nomineeController() { NomineeController
+	 * nominee = new NomineeController(); nominee.setThanksViewName("thanks");
+	 * return nominee; }
+	 */
+
+	@Bean
+	public PropertySourcesPlaceholderConfigurer properties() {
+		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+/*		Resource[] resources = new ClassPathResource[] { new ClassPathResource(
+				"springmvc.properties") };
+		pspc.setLocations(resources);*/
+		pspc.setIgnoreUnresolvablePlaceholders(true);
+
+		Properties properties = new Properties();
+		properties.put("thanksviewname", "nominee/thanks");
+
+		pspc.setProperties(properties);
+
+		return pspc;
+	}
+
 }
---
+++ springcdi/springmvc/src/main/java/com/vamsi/spring/springmvc/controller/NomineeController.java
@@ -0,0 +1,39 @@
+package com.vamsi.spring.springmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.vamsi.spring.beans.Member;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Controller
+@RequestMapping("/nominee")
+public final class NomineeController {
+	private static final Logger log = LoggerFactory.getLogger(Logger.class);
+	private String thanksViewName;
+
+	
+	@Value("${thanksviewname}")
+	public void setThanksViewName(String thanksViewName) {
+		this.thanksViewName = thanksViewName;
+	}
+
+	@RequestMapping(value="/new",method = RequestMethod.GET)
+	public Member form() {
+		return new Member();
+	}
+
+	@RequestMapping(value="/new",method = RequestMethod.POST)
+	public String processFormData(Member member) {
+		log.info("Processing nominee: " + member);
+		log.info("the view name is " + thanksViewName);
+		return thanksViewName;
+	}
+}
